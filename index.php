8c9c2377014b679abdfce2067bc066fb5ec83027
vinsav29 <vinsav29@gmail.com>
2020-06-03 20:54:42 +0300

    simple showing buttons

--- index.php
+++ index.php
@@ -14,45 +14,37 @@
     </style>
 
 </head>
+
 <body onload="scrollBottom()" >
-    <div id ="wrapper">
+<div id ="wrapper">
 
 <h1>Linux services control</h1>
 
 <hr />
 
-<form method='post'>
+<form action="index.php" method='post'>
     <p>NTPD</p>
+    <input type="submit" name="ntpd" value="start">
+    <input type="submit" name="ntpd" value="stop">
+    <input type="submit" name="ntpd" value="restart">
+
     <?php
-    $ntpd_start_off = '
-        <input type="submit" name="ntpd" value="start" disabled>
-        <input type="submit" name="ntpd" value="stop">
-        <input type="submit" name="ntpd" value="restart">
-    ';
-    $ntpd_stop_off = '
-        <input type="submit" name="ntpd" value="start">
-        <input type="submit" name="ntpd" value="stop" disabled>
-        <input type="submit" name="ntpd" value="restart">
-    ';
     $action = $_POST['ntpd'];
     if (!empty($action)) {
         switch ($action)
         {
             case 'start':
-                echo $ntpd_start_off;
-                $output = exec('bash /etc/init.d/ntp start');
+                $output = shell_exec('bash /etc/init.d/ntp start');
                 break;
             case 'stop' :
-                echo $ntpd_stop_off;
                 $output = shell_exec('bash /etc/init.d/ntp stop');
                 break;
             case 'restart' :
-                echo $ntpd_start_off;
                 $output = shell_exec('bash /etc/init.d/ntp restart');
                 break;
         }
     } else {
-        echo $ntpd_start_off;
+//        echo $ntpd_start_off;
     }
     ?>
 </form>
@@ -60,66 +52,58 @@
 <hr />
 
 <form action="index.php" method="post">
-
     <p>GPSD</p>
+    <input type="submit" name="gpsd" value="start">
+    <input type="submit" name="gpsd" value="stop">
+    <input type="submit" name="gpsd" value="restart">
+
     <?php
-    $gpsd_start_off = '
-        <input type="submit" name="gpsd" value="start" disabled>
-        <input type="submit" name="gpsd" value="stop">
-        <input type="submit" name="gpsd" value="restart">
-    ';
-    $gpsd_stop_off = '
-        <input type="submit" name="gpsd" value="start">
-        <input type="submit" name="gpsd" value="stop" disabled>
-        <input type="submit" name="gpsd" value="restart">
-    ';
     $action = $_POST['gpsd'];
     if (!empty($action)) {
+        unset($_POST);
         switch ($action)
         {
             case 'start':
-                echo $gpsd_start_off;
                 $output = shell_exec('bash /etc/init.d/gpsd start');
                 break;
             case 'stop' :
-                echo $gpsd_stop_off;
-//                echo 'gpsd.service stop';
                 $output = shell_exec('bash /etc/init.d/gpsd stop');
                 break;
             case 'restart' :
-                echo $gpsd_start_off;
-//                echo 'gpsd.service restart';
                 $output = shell_exec('bash /etc/init.d/gpsd restart');
                 break;
         }
     } else {
-    echo $gpsd_start_off;
+//    echo $gpsd_start_off;
     }
+
     ?>
 </form>
 
 <hr />
+
 <form method="post">
-    <input type="submit" name="clear" value="Clear logs">
-    <p></p>
+    <p>Logs</p>
     <div id="log" class="div_log" >
         <?php
         $action = $_POST['clear'];
         if (!empty($action)) {
             $log_file = fopen("log.txt", "w");
             fclose($log_file);
+            unset($_POST);
         }
         if (!empty($output)) {
             $log_file = fopen("log.txt", "a+");
             $time = date("Y-m-d H:i:s");
-            fwrite($log_file, $time."\n");
-            fwrite($log_file, $output."\n");
+            fwrite($log_file, $time."\r\n".$output."\r\n");
             $log = file_get_contents("log.txt",true);
             echo "<pre>$log</pre>";
             fclose($log_file);
         }
         ?>
     </div>
+    <p></p>
+    <input type="submit" name="clear" value="Clear logs">
 </form>
 
 <script>
@@ -128,8 +112,8 @@
     }
 </script>
 
-        </div>
-    </body>
+</div>
+</body>
 </html>
 
 
